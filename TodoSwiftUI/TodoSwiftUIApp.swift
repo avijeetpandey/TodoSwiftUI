fb3fa55d5cee80c7145b43f9a1257b2b7f1ea08a
Avijeet Pandey <avijeetpandey25@gmail.com>
2022-10-28 15:50:00 +0530

    Initial Commit

---
+++ TodoSwiftUI/TodoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoSwiftUIApp.swift
+//  TodoSwiftUI
+//
+//  Created by Avijeet Pandey on 28/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
